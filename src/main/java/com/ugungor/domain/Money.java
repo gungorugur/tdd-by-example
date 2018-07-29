3095841e84013d0edfb4277abd67585a98affbda
Ugur Gungor <ugungor@kocfinans.com.tr>
2018-07-29 22:00:41 +0300

    #refactor, add base  method and add factory method.

--- src/main/java/com/ugungor/domain/Money.java
+++ src/main/java/com/ugungor/domain/Money.java
@@ -1,12 +1,18 @@
 package com.ugungor.domain;
 
-public class Money {
+public abstract class Money {
 
     protected Integer amount;
 
     public Money(Integer amount) {
         this.amount = amount;
     }
+
+    public static Money dollar(Integer amount) {
+        return new Dollar(amount);
+    }
+
+    public abstract Money times(Integer multiplier);
 
     @Override
     public boolean equals(Object object) {
--- src/test/java/com/ugungor/DollarTest.java
+++ src/test/java/com/ugungor/DollarTest.java
@@ -1,7 +1,7 @@
 package com.ugungor;
 
-import com.ugungor.domain.Dollar;
 import com.ugungor.domain.Franc;
+import com.ugungor.domain.Money;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -13,18 +13,18 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
 
-        final Dollar five = new Dollar(5);
-        assertThat(five.times(2), is(new Dollar(10)));
-        assertThat(five.times(3), is(new Dollar(15)));
+        final Money five = Money.dollar(5);
+        assertThat(five.times(2), is(Money.dollar(10)));
+        assertThat(five.times(3), is(Money.dollar(15)));
     }
 
     @Test
     public void testEquality() {
 
-        assertThat(new Dollar(5).equals(new Dollar(5)), is(Boolean.TRUE));
-        assertThat(new Dollar(5).equals(new Dollar(6)), is(Boolean.FALSE));
+        assertThat(Money.dollar(5).equals(Money.dollar(5)), is(Boolean.TRUE));
+        assertThat(Money.dollar(5).equals(Money.dollar(6)), is(Boolean.FALSE));
         assertThat(new Franc(5).equals(new Franc(5)), is(Boolean.TRUE));
         assertThat(new Franc(5).equals(new Franc(6)), is(Boolean.FALSE));
-        assertThat(new Franc(5).equals(new Dollar(5)), is(Boolean.FALSE));
+        assertThat(new Franc(5).equals(Money.dollar(5)), is(Boolean.FALSE));
     }
 }
