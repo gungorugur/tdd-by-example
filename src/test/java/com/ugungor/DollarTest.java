6793a4a50c7bffbc1f63b7a9fb22d4e5d896831e
Ugur Gungor <ugungor@kocfinans.com.tr>
2018-07-29 21:41:10 +0300

    fail test, dollars are francs

--- src/test/java/com/ugungor/DollarTest.java
+++ src/test/java/com/ugungor/DollarTest.java
@@ -1,6 +1,7 @@
 package com.ugungor;
 
 import com.ugungor.domain.Dollar;
+import com.ugungor.domain.Franc;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -22,5 +23,8 @@ public class DollarTest {
 
         assertThat(new Dollar(5).equals(new Dollar(5)), is(Boolean.TRUE));
         assertThat(new Dollar(5).equals(new Dollar(6)), is(Boolean.FALSE));
+        assertThat(new Franc(5).equals(new Franc(5)), is(Boolean.TRUE));
+        assertThat(new Franc(5).equals(new Franc(6)), is(Boolean.FALSE));
+        assertThat(new Franc(5).equals(new Dollar(5)), is(Boolean.FALSE));
     }
 }
